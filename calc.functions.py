f88512d6f293c828045e8e91e64d4321dfb28c23
John Fernandez <jcsnowrider@hotmail.com>
2018-02-23 20:48:25 -0500

    calc.functions.py git push git commit -m calc.functions.py

---
+++ calc.functions.py
@@ -0,0 +1,12 @@
+# This function adds, subtracts, multiplies and divides two numbers 
+def add(x, y):
+   return x + y
+
+def subtract(x, y):
+   return x - y
+
+def multiply(x, y):
+   return x * y
+
+def divide(x, y):
+   return x / y
