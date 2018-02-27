a9373afd0caff622fb9369ffa327ead499abc6b5
John Fernandez <jcsnowrider@hotmail.com>
2018-02-26 19:11:29 -0500

    Initial version of a simple guessing game.

---
+++ guessing-game.py
@@ -0,0 +1,13 @@
+answer = "Geurilla"
+
+while True:
+    print("Guess an animal I am thinking of")
+    if input() == answer:
+        print("Congradulations, you guessed the animal I was thinking of")
+
+        break
+    else:
+        print("Give it another try")
+
+    
+    
