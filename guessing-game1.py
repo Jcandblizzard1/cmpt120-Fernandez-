3da53333f0f1b67d560ce16eb48012e811077354
John Fernandez <jcsnowrider@hotmail.com>
2018-03-01 20:46:55 -0500

    Usability improvements to the guessing game, resolves #2 and #3.

---
+++ guessing-game1.py
@@ -0,0 +1,14 @@
+answer = "gorilla"
+guess = input("Guess an animal? ")
+#Guess an animal and get the correct answer
+while True:
+    if guess == "quit":
+        print("Goodbye")
+        break
+    elif guess == answer:
+        print("Congrats you guessed it right! ")
+        break
+    else:
+        guess = input("Give it another try ")
+        
+
